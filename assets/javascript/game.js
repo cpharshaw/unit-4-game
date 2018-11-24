f886a9f46a69f407122c97b0d2bf49302765a9cd
cpharshaw <cpharshaw+git@gmail.com>
2018-11-24 00:45:51 -0500

    started basic jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,29 @@
+$(document).ready(function () {
+
+    var selectedChar;
+    var otherChars;
+    var enemyChar;
+    var charChosen = 0;
+
+    $(".charBox").on("click", function () {
+
+        if (charChosen < 1) {
+            charChosen++;
+            selectedChar = $(this);
+            $("#charInstruct").text("Choose an enemy to battle...");
+            $(this).remove();
+            $("#yourChar").append(selectedChar);
+        } else if (charChosen < 2) {
+            charChosen++;
+            enemyChar = $(this);
+            $(".charPick").remove();
+            $("#enemyChar").append(enemyChar);
+            
+        }
 
 
+    })
+
+
+
+});
